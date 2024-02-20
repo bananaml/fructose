66fc45a6267dbb3111624811fb9a120a7d68459f
Aaron Peddle <peddle.aaron@gmail.com>
2024-02-19 21:09:23 -0900

    make some evals harder, swap to 3.5 as default, and tweak enum prompt to work more consistently in edge cases

--- eval/elderberry_eval.py
+++ eval/elderberry_eval.py
@@ -2,9 +2,14 @@ from enum import Enum
 from core import FructoseEval
 from dataclasses import dataclass
 
+class Color(Enum):
+    LAVENDER = "lavender"
+    INDIGO = "indigo"
+    TERRACOTTA = "terracotta"
+
 @dataclass
 class House:
-  color: str
+  color: Color
   size: int
   is_occupied: bool 
 
@@ -23,10 +28,11 @@ class Player:
   name: str
   mana: int
 
-class Color(Enum):
-    LAVENDER = "lavender"
-    INDIGO = "indigo"
-    TERRACOTTA = "terracotta"
+@dataclass
+class Marriage:
+    name: str
+    people: tuple[Person, Person]
+    date: str
 
 
 class ElderberryEval(FructoseEval):
@@ -159,3 +165,14 @@ class ElderberryEval(FructoseEval):
           """
 
         write_github_stars_badge_html("bananaml/fructose")
+
+    @staticmethod
+    def eval_wedding(ai, debug):
+        @ai(debug=debug)
+        def makeup_marriage() -> Marriage:
+            """
+            creates a made up marriage
+            """
+
+        makeup_marriage()
+
