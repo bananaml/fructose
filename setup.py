89dc06deb806c72c58008d24a718bc53edc7056b
Nik <nikita.logachev@me.com>
2024-02-20 13:51:31 -0900

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fructose',
-    version='0.0.3',
+    version='0.0.4',
     packages=["fructose"],
     package_dir={'': 'src'},
     install_requires=[
