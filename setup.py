334d3b468ca3036e3e49328c446ea3832425a91e
Nik <nikita.logachev@me.com>
2024-02-26 15:30:00 +0100

    Fix needless import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fructose',
-    version='0.0.5',
+    version='0.0.6',
     packages=["fructose"],
     package_dir={'': 'src'},
     install_requires=[
