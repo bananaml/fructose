3c5da65e7078d06ce271e06af55f7ebeb5e3a189
Nik <6206742+nik-418@users.noreply.github.com>
2024-04-01 13:49:46 +0200

    AGI (#23)

---
+++ examples/human.py
@@ -0,0 +1,14 @@
+from fructose import Fructose
+
+human = Fructose()
+
+@human()
+def repeat(input_str: str) -> str:
+    """
+    repeat back the input_str
+    """
+    ...
+
+print(repeat("Ahoy"))
+
+
